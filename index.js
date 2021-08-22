cc8db875deb9a2809192865a812a2e3c688a2cd3
Wamackie <wamackie8456@gmail.com>
2021-08-21 23:04:03 -0400

    finished list

--- index.js
+++ index.js
@@ -5,23 +5,54 @@ const data = () => {
     return inquirer.prompt([
     {
         type: 'input',
+        name: 'title',
+        message: 'What is the name of the project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write the project description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What should be typed to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instruct others how to use your project:'
+    },
+    {
+        type: 'input',
+        name: 'licenses',
+        message: 'What license does your project use?',
+        choices: ['x', 'y', 'z']
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What should be typed to run tests?'
+    },
+    {
+        type: 'input',
         name: 'creator',
         message: 'Who created this?'
     },
     {
         type: 'input',
-        name: 'username',
+        name: 'github',
         message: 'What is the associated GitHub username?'
     },
     {
         type: 'input',
         name: 'email',
         message: 'What email address can be used to contact you?'
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of the project?'
-    },
+    }
     ])
 }
