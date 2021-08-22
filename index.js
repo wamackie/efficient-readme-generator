6eb6aafe00286b0e772218e440f0835185fc7763
Wamackie <wamackie8456@gmail.com>
2021-08-22 17:41:02 -0400

    changed file structure

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateReadMe = require('./src/readme_template')
+const util = require('util');
+//const createReadMe = util.promisify(fs.writeFile);
 
-const promptUser = () => {
-    return inquirer.prompt([
+const logData = () => inquirer.prompt([
     {
         type: 'input',
         name: 'title',
@@ -136,4 +136,36 @@ const promptUser = () => {
         }
     }
     ])
-}
+
+    module.exports = (title, description, installation, usage, licenses, contribute, test, creator, github, email) => {
+        return `# ${data.title}
+    ${data.description}
+    
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## Installation
+    Console command to install dependencies:
+    ${data.installation}
+    
+    ## Usage:
+    ${data.usage}
+    
+    ## License:
+    ${data.licenses}
+    
+    ## Contributing:
+    ${data.contribute}
+    
+    ## Tests:
+    ${data.test}
+    
+    ## Contact:
+    This project was created by ${data.creator}. Feel free to contact me at https://github.com/${data.github} or by email at ${data.email}.
+    `
+    }
